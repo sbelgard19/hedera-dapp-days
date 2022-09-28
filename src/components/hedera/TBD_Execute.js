b9be43282598396b01c25174507be247c3722120
Scott Belgard <belgard.scott@gmail.com>
2022-09-28 14:21:48 -0400

    Add files via upload
    test TBD

---
+++ src/components/hedera/TBD_Execute.js
@@ -0,0 +1,9 @@
+
+async function TBD_ExecuteFcn() {
+	console.log(`\n=======================================`);
+	console.log(`- Executing the TBD...`);
+
+	return 9999;
+}
+
+export default TBD_ExecuteFcn;
