01f06694ebeac296891d910ad2e38300cf6b2e65
Scott Belgard <belgard.scott@gmail.com>
2022-09-30 14:56:25 -0400

    Update MyDoubleButton.jsx

--- src/components/MyDoubleButton.jsx
+++ src/components/MyDoubleButton.jsx
@@ -6,11 +6,10 @@ function MyDoubleButton(props) {
 	return (
 		<div>
 {/* 			<MyButton fcn={props.fcn1} buttonLabel={props.buttonLabel1} /><MyButton fcn={props.fcn1} buttonLabel={props.buttonLabel1} /> */} 
-	<button onClick={props.fcn1} className="cta-button">{props.buttonLabel1}</button> side-by-side buttons 
-
-< text={props.text1}/>< text={props.text2}/>
-	
-	<button onClick={props.fcn2} className="cta-button">{props.buttonLabel2}</button>  
+		
+<button onClick={props.fcn1} className="cta-button">{props.buttonLabel1}</button>side-by-side buttons<button onClick={props.fcn2} className="cta-button">{props.buttonLabel2}</button>  
+		
+<MyText props.text1}/><MyText text={props.text2}/>
 		</div>
 	);
 
