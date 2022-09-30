335cce83b96bf1117bf0689818fff03ed2a2e83e
Scott Belgard <belgard.scott@gmail.com>
2022-09-30 11:01:01 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -194,7 +194,6 @@ function App() {
 				buttonLabel={"Clear Messages"}
 			/>
 
-  <legend>Personalia:</legend>
   Name: <input type="text"><br>
   Email: <input type="text"><br>
   Date of birth: <input type="text">
