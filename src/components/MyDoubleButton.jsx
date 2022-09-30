a41fd2c70a1e614fc7f465b931a8a0f558454c81
Scott Belgard <belgard.scott@gmail.com>
2022-09-30 12:59:24 -0400

    Update MyDoubleButton.jsx

--- src/components/MyDoubleButton.jsx
+++ src/components/MyDoubleButton.jsx
@@ -5,11 +5,8 @@ import MyText from "./MyText.jsx";
 
 function MyDoubleButton(props) {
 	return (
-		
-{/* A JSX comment */}
-
 		<div>
-{/* 			<MyButton fcn={props.fcn1} buttonLabel={props.buttonLabel1} /><MyButton fcn={props.fcn1} buttonLabel={props.buttonLabel1} /> */}
+//			<MyButton fcn={props.fcn1} buttonLabel={props.buttonLabel1} /><MyButton fcn={props.fcn1} buttonLabel={props.buttonLabel1} /> 
 		<button onClick={props.fcn1} className="cta-button">{props.buttonLabel1}</button> NEXT <button onClick={props.fcn1} className="cta-button">{props.buttonLabel1}</button>  
 		</div>
 	);
