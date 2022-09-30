f75e0bad15a4628c34fb3eae965ffe6adde6b935
Scott Belgard <belgard.scott@gmail.com>
2022-09-30 11:43:27 -0400

    Update MyGroup.jsx

--- src/components/MyGroup.jsx
+++ src/components/MyGroup.jsx
@@ -9,6 +9,13 @@ function MyGroup(props) {
 			<MyButton fcn={props.fcn} buttonLabel={props.buttonLabel} />
 		</div>
 	);
+function MyDoubleButton(props) {
+	return (
+		<div>
+			<MyButton fcn={props.fcn} buttonLabel={props.buttonLabel} /><MyButton fcn={props.fcn} buttonLabel={props.buttonLabel} />
+		</div>
+	);
+
 }
 
 export default MyGroup;
